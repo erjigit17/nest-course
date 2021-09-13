98e6d80f11bb88a7bc2c0157b483f41b1de25693
Эржигит Имамов <erjigit17@gmail.com>
2021-09-13 14:21:02 +0600

    add dto

--- src/users/dto/creatr-user.dto.ts
+++ src/users/dto/creatr-user.dto.ts
@@ -1,7 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 
 
 export class CreateUserDto {
+  @ApiProperty({example: 'user@email.com', description: 'email address'})
   readonly email: string;
+  @ApiProperty({example: '12345678', description: 'user password'})
   readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,24 @@
 import { Body, Controller, Post, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/creatr-user.dto';
 import { UsersService } from './users.service';
+import { User } from './users.models';
 
 @Controller('users')
 export class UsersController {
 
   constructor (private usersService: UsersService) {}
 
+  @ApiOperation({summary: 'Create new user'})
+  @ApiResponse({status: 200, type: User})
   @Post()
   creata(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
   }
 
+  
+  @ApiOperation({summary: 'Get all users'})
+  @ApiResponse({status: 200, type: [User]})
   @Get()
   getAll(){
     return this.usersService.getAllUsers()
